7e4cc7ad76b2d21f0ced59f3a130083b8b28c943
Zohirul369 <zohirul369@gmail.com>
2025-07-19 23:11:18 +0600

    account setting show, can edit profile info and update

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -52,3 +52,32 @@ function showChangeProfilePicture() {
     });
   }, 100);
 }
+
+function showProfile() {
+  document.getElementById("contentArea").innerHTML = `
+    <div class="card border-0 shadow mb-4">
+      <div class="card-body p-4">
+        <h3 class="fs-4 mb-3">My Profile</h3>
+        <form>
+          <div class="mb-3">
+            <label class="form-label">Name*</label>
+            <input type="text" class="form-control" placeholder="Enter Name">
+          </div>
+          <div class="mb-3">
+            <label class="form-label">Email*</label>
+            <input type="email" class="form-control" placeholder="Enter Email">
+          </div>
+          <div class="mb-3">
+            <label class="form-label">Designation*</label>
+            <input type="text" class="form-control" placeholder="Your Designation">
+          </div>
+          <div class="mb-3">
+            <label class="form-label">Mobile*</label>
+            <input type="text" class="form-control" placeholder="Your Mobile">
+          </div>
+          <button type="submit" class="btn btn-primary">Update</button>
+        </form>
+      </div>
+    </div>
+  `;
+}
